6c001faa2f509c542853afd333e0f20d17df5d86
LuisMarin <73456865+luismarin98@users.noreply.github.com>
2023-11-08 23:23:18 +0100

    updates

--- src/features/User/components/userForm.tsx
+++ src/features/User/components/userForm.tsx
@@ -6,17 +6,10 @@ import InputText from './inputText';
 import ModalInsert from './modalInsert';
 
 export const UserForm: FC = () => {
-    const { getSearch, saveUser } = useContext(UsuariosContext) as IUsuariosContext;
+    const { saveUser } = useContext(UsuariosContext) as IUsuariosContext;
     const { getValues } = useFormContext<UserRequest>();
 
-    const [openSearch, setOpenSearch] = useState(false);
     const [openSave, setOpenSave] = useState(false);
-
-    const handleUser = async () => {
-        const value = { ...getValues() };
-        if (!value.id) return null;
-        getSearch(value.id);
-    };
 
     const handleSave = async () => {
         const value = { ...getValues() };
@@ -24,15 +17,11 @@ export const UserForm: FC = () => {
         saveUser(value)
     }
 
-    const toggleModalSearch = () => {
-        setOpenSearch(!openSearch);
-    };
-
     const toggleModalSave = () => {
-        setOpenSearch(!setOpenSave);
+        setOpenSave(!openSave);
     };
 
-    return <div className="container flex flex-row shadow-sm shadow-slate-600 gap-1 p-2 rounded-md bg-stone-100 dark:bg-slate-800  justify-center">
+    return <div className="container flex flex-col shadow-sm shadow-slate-600 gap-1 p-2 rounded-md bg-stone-100 dark:bg-slate-800  justify-center">
         <ModalInsert
             isOpen={openSave}
             onClose={toggleModalSave}
@@ -50,33 +39,12 @@ export const UserForm: FC = () => {
             </div>
         </ModalInsert>
 
-        <ModalInsert
-            isOpen={openSearch}
-            onClose={toggleModalSearch}
-            title={"Buscar Usuario"}
-        >
-            <div className="grid p-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2 text-center ">
-                <InputText name='id' title={'Ingresa el id del usuario'} />
-                <button className="p-1 hover:bg-slate-400 rounded-sm w-32 dark:bg-slate-200" onClick={handleUser}>Buscar</button>
-            </div>
-        </ModalInsert>
-
         <div className="lg:text-right md:text-center">
             <button
                 title="boton"
-                className="border  rounded-xl p-2 m-2 bg-blue-400"
-                onClick={toggleModalSearch}
-            >Consultar</button>
-            <button
-                title="boton"
                 onClick={toggleModalSave}
-                className="border  rounded-xl p-2 m-2 bg-blue-400 "
+                className="border  rounded-sm p-2 m-2 bg-blue-400 "
             >Nuevo Usuario</button>
         </div>
     </div>
 }
-
-/**
- * 
-
- */
