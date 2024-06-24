9bbc583a5287533f2f5e77a4e8492c4ce0eedde2
LuisMarin <73456865+luismarin98@users.noreply.github.com>
2024-06-24 10:00:16 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,7 @@ const NavBar = () => {
     }, []);
 
     return (
-        <Navbar className="mx-auto max-w-screen-xl px-6 py-3">
+        <Navbar className="mx-auto max-w-screen-xl px-6 py-3 bg-neutral-50 dark:bg-neutral-800">
             <div className="flex items-center justify-between text-blue-gray-900">
                 <Link to='/'>
                     <Typography as="a" variant="h6" className="mr-4 cursor-pointer py-1.5">Test Intuito</Typography>
